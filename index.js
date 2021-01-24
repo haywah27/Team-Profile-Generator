71dab946cd1296a0826fc2fc8c8b24c6714e507e
haywah27 <hayleywahlroos15@gmail.com>
2021-01-23 22:05:59 -0800

    developing inquiries

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const markdown = require("./generateProfile");
+const profile = require("./generateProfile");
+
+
+const questions = [
+    {
+        type: "input",
+        message: "What is the employee name?",
+        name: "name",
+      },
+      {
+        type: "list",
+        message: "What best describes their position?",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is their emplooyee ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is their email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is their office number?",
+        name: "office",
+      }
+];
+
+
+
+
+function writeToFile(fileName, data) {
+    
+    fs.writeFile(fileName, data, err =>
+    err ? console.error(err) : console.log("Success! New README generated!"));
+
+}
+
+function init() {
+
+    inquirer.prompt(questions).then(
+        answers => {
+            console.log(answers);
+            // badge = markdown.renderBadge(answers.license);
+            // badgeLink = markdown.renderLink(answers.license);
+            // licenseSection = markdown.renderSection(answers.license);
+            
+            writeToFile("index.html", profile.manager(answers.name, answers.title, answers.id, answers.email, answers.office));
+        })
+    
+}
+
+init();
